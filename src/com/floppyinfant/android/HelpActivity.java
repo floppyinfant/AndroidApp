f520ca12604e94d3c43cfefd38a9618dbb285ea6
unknown <TM@.(none)>
2012-04-21 02:33:35 +0200

    Hilfe (HelpActivity) benutzt jetzt den asset-Ordner.

--- src/com/floppyinfant/android/HelpActivity.java
+++ src/com/floppyinfant/android/HelpActivity.java
@@ -2,115 +2,59 @@ package com.floppyinfant.android;
 
 import java.io.IOException;
 import java.io.InputStream;
-
 import android.app.Activity;
 import android.content.Context;
+import android.content.res.AssetManager;
 import android.os.Bundle;
-import android.util.Log;
 import android.webkit.WebView;
 
-/**
- * @author Arno Becker, 2010 visionera gmbh
- */
 public class HelpActivity extends Activity {
 
-  /** Kuerzel fuers Logging. */
-  private static final String TAG = HelpActivity.class.getSimpleName();
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.help);
 
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    Log.v(TAG, "onCreate(): entered...");
-    setContentView(R.layout.help);
+		final WebView view = (WebView) findViewById(R.id.webview);
+		view.getSettings().setJavaScriptEnabled(true);
+		initWebView(view, this);
+		// dies hier ginge auch:
+		// view.loadUrl("http://www.visionera.de");
+		// view.loadUrl("file:///android_asset/www/index.html");	// PhoneGap: Class DroidGap.class
+		view.bringToFront();
+	}
 
-    final WebView view = (WebView) findViewById(R.id.webview);
-    view.getSettings().setJavaScriptEnabled(true);
-    // dies hier ginge auch:
-    // view.loadUrl("http://www.visionera.de");
-    // view.loadUrl("file:///android_asset/www/index.html");	// PhoneGap: Class DroidGap.class
-    initialisiereWebKitEinfach(view, this);
-    view.bringToFront();
-  }
+	/**
+	 * Initialisiert WebKit mit einer HTML-Seite aus dem
+	 * Ressourcen-Ordner.
+	 * 
+	 * @param view WebView zur Darstellung von Webinhalten
+	 * @param context Kontext der Anwendung
+	 * @param res raw-ressource (HTML-Seite in Directory /res/raw)
+	 */
+	private void initWebView(final WebView view, final Context context) {
+		final String mimetype = "text/html";
+		final String encoding = "UTF-8";
+//		final int res = R.raw.index;
+		final String url = "www/index.html";
 
-  /*private void initialisiereWebKit(WebView view, Context context) {
-    final String mimetype = "text/html";
-    final String encoding = "UTF-8";
-    String htmldata;
-    
-    // Welche Hilfe soll angezeigt werden? 
-    // Default: vollständige Hilfeseite
-    final Bundle extras = getIntent().getExtras();
-    int contextMenueId = 0;
-    InputStream is = null;
-    if (extras != null) {
-      if (extras.containsKey(
-          Startseite.CONTEXTMENUE_HILFE)) {
-        contextMenueId = 
-          extras.getInt(Startseite.CONTEXTMENUE_HILFE);
-      } 
-    } else {
-      contextMenueId = R.raw.hilfe_komplett;
-    } 
-    
-    is = context.getResources()
-        .openRawResource(contextMenueId);
+		// Folder /res/raw
+//		final InputStream ins = context.getResources().openRawResource(res);
+		
+		// Folder /assets
+		AssetManager assetManager = this.getAssets();
+		InputStream ins;
+		
+		try {
+			ins = assetManager.open(url);
+			if (ins != null && ins.available() > 0) {
+				final byte[] bytes = new byte[ins.available()];
+				ins.read(bytes);
+				String html = new String(bytes);
+				view.loadDataWithBaseURL(null, html, mimetype, encoding, null);
+			}
+		} catch (IOException e) {
 
-    try {
-      if (is != null && is.available() > 0) {
-        final byte[] bytes = new byte[is.available()];
-        is.read(bytes);
-        htmldata = new String(bytes);
-        // Log.v(TAG, "initWebkit(): HTML der Hilfeseite: "
-        // + htmldata);
-
-        // WICHTIGER HINWEIS:
-        // Workaround fuer Bug in Android (bis min. Version
-        // 1.5!):
-        // view.loadData(htmldata, mimetype, encoding)
-        // funktioniert nicht!
-        // WebView haengt sich an '<style type="text/css">'
-        // auf. Saemtliche
-        // CSS-Definitionen werden ignoriert und es kommt zu
-        // einer Fehlermeldung
-        // im WebView.
-        // Loesung: loadDataWithBaseURL funktioniert!
-
-        // view.loadData(htmldata, mimetype, encoding);
-        view.loadDataWithBaseURL(null, htmldata, mimetype,
-            encoding, null);
-
-        Log.v(TAG, "initWebkit(): HTML wird angezeigt...");
-      }
-    } catch (IOException e) {
-      Log.v(TAG, "initWebkit(): Fehler: " + e.toString());
-    }
-  } */
-  
-  /**
-   * Initialisiert WebKit mit einer HTML-Seite aus dem
-   * Ressourcen-Ordner.
-   * 
-   * @param view WebView zur Darstellung von 
-   *     Webinhalten.
-   * @param context Kontext der Anwendung
-   */
-  private void initialisiereWebKitEinfach(
-    final WebView view, final Context context) {
-    final String mimetype = "text/html";
-    final String encoding = "UTF-8";
-    String htmldata;
-    
-    final InputStream is = context.getResources().openRawResource(R.raw.index);    // TODO: use assets<DIR>
-
-    try {
-      if (is != null && is.available() > 0) {
-        final byte[] bytes = new byte[is.available()];
-        is.read(bytes);
-        htmldata = new String(bytes);
-        view.loadDataWithBaseURL(null, htmldata, mimetype, encoding, null);
-      }
-    } catch (IOException e) {
-    	
-    }
-  }
+		}
+	}
 }
--- src/com/floppyinfant/android/MainActivity.java
+++ src/com/floppyinfant/android/MainActivity.java
@@ -3,6 +3,7 @@ package com.floppyinfant.android;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.ContextMenu;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -10,98 +11,107 @@ import android.view.View;
 import android.view.ContextMenu.ContextMenuInfo;
 
 public class MainActivity extends Activity {
-	
-	/* 
+
+	/** 
+	 * Logging
+	 */
+	private static final String TAG = MainActivity.class.getSimpleName();
+
+	/* ************************************************************************
 	 * Lifecycle
 	 */
-    /** 
-     * Called when the activity is first created.
-     */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-        
-        registerForContextMenu(findViewById(R.id.sf_action));
-        
-    }
-    
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+
+		registerForContextMenu(findViewById(R.id.sf_action));
+
+		Log.v(TAG, "onCreate");
+	}
+
 	@Override
 	protected void onResume() {
 		// TODO Auto-generated method stub
 		super.onResume();
 	}
 
-    @Override
+	@Override
 	protected void onPause() {
 		// TODO Auto-generated method stub
 		super.onPause();
 	}
-    
-	/* 
-     * Actions
-     */
-    /** 
-     * Button onClick
-     */
-    public void onClickAction(final View view) {
-    	switch (view.getId()) {
-    	case R.id.sf_action:
-    		startActivity(new Intent(this, SubActivity.class));
-    		break;
-    	}
-    }
-    
-    /* 
-     * Menues(non-Javadoc)
-     * @see android.app.Activity#onCreateOptionsMenu(android.view.Menu)
-     */
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-      super.onCreateOptionsMenu(menu);
-      getMenuInflater().inflate(R.menu.main, menu);    
-      return true;
-    }
-      
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-      switch (item.getItemId()) {
-        case R.id.opt_preferences:
-//          startActivity(new Intent(this, EinstellungenBearbeiten.class));
-          return true;
-        case R.id.opt_help:
-          startActivity(new Intent(this, HelpActivity.class));
-          return true;
-        case R.id.opt_close:
-          finish();
-          return true;
-        default:
-          return super.onOptionsItemSelected(item);
-      }
-    }
 
-    @Override
+	/* ************************************************************************
+	 * Actions
+	 */
+	/** 
+	 * Button onClick
+	 */
+	public void onClickAction(final View view) {
+		switch (view.getId()) {
+		case R.id.sf_action:
+			// Seite wechseln
+			startActivity(new Intent(this, SubActivity.class));
+			break;
+		}
+	}
+
+	/* ************************************************************************
+	 * Menues(non-Javadoc)
+	 * @see android.app.Activity#onCreateOptionsMenu(android.view.Menu)
+	 */
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		super.onCreateOptionsMenu(menu);
+		getMenuInflater().inflate(R.menu.main, menu);    
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case R.id.opt_preferences:
+//          startActivity(new Intent(this, EinstellungenBearbeiten.class));
+			return true;
+		case R.id.opt_help:
+			startActivity(new Intent(this, HelpActivity.class));
+			return true;
+		case R.id.opt_close:
+			finish();
+			return true;
+		default:
+			return super.onOptionsItemSelected(item);
+		}
+	}
+
+	@Override
 	public boolean onPrepareOptionsMenu(Menu menu) {
-//		 menu.findItem(R.id.opt_help).setEnabled(false);
+//		menu.findItem(R.id.opt_help).setEnabled(false);
 		return super.onPrepareOptionsMenu(menu);
 	}
-    
-    @Override
-    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenuInfo menuInfo) {
-      if (v.getId() == R.id.sf_action) {
-    	  getMenuInflater().inflate(R.menu.main_contextmenue, menu);
-      }
-      super.onCreateContextMenu(menu, v, menuInfo);
-    }
-    
+
 	@Override
-    public boolean onContextItemSelected(MenuItem item) {
-      if (item.getItemId() == R.id.opt_help) {
-//        final Intent intent = new Intent(this, HilfeAnzeigen.class);
-//        intent.putExtra(CONTEXTMENUE_HILFE, R.raw.hilfe_startseite_geokontakte);
-//        startActivity(intent);
-        return true;
-      }
-      return super.onContextItemSelected(item);   
-    }
+	public void onCreateContextMenu(ContextMenu menu, View v, ContextMenuInfo menuInfo) {
+		if (v.getId() == R.id.sf_action) {
+			getMenuInflater().inflate(R.menu.main_contextmenue, menu);
+		}
+		super.onCreateContextMenu(menu, v, menuInfo);
+	}
+
+	@Override
+	public boolean onContextItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case R.id.ctx_help:
+//          final Intent intent = new Intent(this, HilfeAnzeigen.class);
+//          intent.putExtra(CONTEXTMENUE_HILFE, R.raw.hilfe_startseite_geokontakte);
+//          startActivity(intent);
+			startActivity(new Intent(this, HelpActivity.class));
+			return true;	// zeigt dem Framework an, dass das Ereignis verbraucht ist
+		default:
+			// return true;
+		}
+		
+		return super.onContextItemSelected(item);   
+	}
 }
