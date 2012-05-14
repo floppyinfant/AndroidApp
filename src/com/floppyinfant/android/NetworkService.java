cd7cfa67768e22fe653ba17989bc8d1368d596ce
unknown <TM@.(none)>
2012-05-14 14:32:42 +0200

    Service Handler with Message-Queue

--- src/com/floppyinfant/android/NetworkService.java
+++ src/com/floppyinfant/android/NetworkService.java
@@ -3,16 +3,31 @@ package com.floppyinfant.android;
 import android.app.Service;
 import android.content.Intent;
 import android.os.Binder;
+import android.os.Bundle;
+import android.os.Handler;
 import android.os.IBinder;
+import android.os.Message;
 
 public class NetworkService extends Service {
 	
 	private IBinder mBinder = new ServiceBinder();
+	/** Callback Handler */
+	private Handler mHandler;
+	
 	private volatile boolean isBusy;
 	
+	/**
+	 * Service Binder to communicate with the Service
+	 * @author TM
+	 *
+	 */
 	public class ServiceBinder extends Binder {
 		
-		public void send() {
+		public void setCallback(Handler handler) {
+			mHandler = handler;
+		}
+		
+		public void doInBackground() {
 			if (isBusy) {
 				return;
 			}
@@ -20,17 +35,43 @@ public class NetworkService extends Service {
 				@Override
 				public void run() {
 					isBusy  = true;
-					// TODO do something
+					
+					// do something
+					long res = execute();
+					
+					// send result back to the starting Activity
+					Message msg = new Message();
+					Bundle bundle = new Bundle();
+					bundle.putLong("result", res);
+					msg.setData(bundle);
+					mHandler.sendMessage(msg);
 					
 					isBusy = false;
 				}
+
 			}.start();
+		}
+		
+		public void send() {
+			// TODO do something
+			
 		}
 		
 		public void receive() {
 			// TODO do something
 			
 		}
+	} // end Binder
+	
+
+	/**
+	 * called by a Thread
+	 * @return result
+	 */
+	private long execute() {
+		// TODO calculation of result
+		
+		return -777;
 	}
 
 	@Override
